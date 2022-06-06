606e40d081eb4400b932e752c8832639615afa49
joel.jayoma <joel.jayoma@ing.com>
2022-06-06 21:39:45 +0800

    added images results component

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import SearchHeader from "../components/SearchHeader";
 import SearchResults from "../components/SearchResults";
 import Response from "../Response";
 import { useRouter } from "next/router";
+import ImageResults from "../components/ImageResults";
 
 export default function Search({ results }) {
   console.log(results);
@@ -17,7 +18,12 @@ export default function Search({ results }) {
       <SearchHeader />
 
       {/* Search Results */}
-      <SearchResults results={results} />
+
+      {router.query.searchType === "image" ? (
+        <ImageResults results={results} />
+      ) : (
+        <SearchResults results={results} />
+      )}
     </div>
   );
 }
